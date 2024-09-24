19af15dae9cef6014b7c89de29ecc657570e6730
Anlprnc <anilpirincciii@gmail.com>
2024-09-24 23:31:59 +0300

    finished

---
+++ Movie/Model/MovieDetail.swift
@@ -0,0 +1,8 @@
+//
+//  MovieDetail.swift
+//  Movie
+//
+//  Created by Anıl on 24.09.2024.
+//
+
+import Foundation
---
+++ Movie/ViewModel/MovieDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MovieDetailViewModel.swift
+//  Movie
+//
+//  Created by Anıl on 24.09.2024.
+//
+
+import Foundation
